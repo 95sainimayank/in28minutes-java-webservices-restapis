28b22ea21f2dee0f4971561ab8180211af07a4b2
95sainimayank <95sainimayank@gmail.com>
2021-08-01 05:59:31 +0530

    initial commit

---
+++ src/main/java/com/in28minutes/soap/webservies/soapcoursemanagement2/SoapCourseManagement2Application.java
@@ -0,0 +1,13 @@
+package com.in28minutes.soap.webservies.soapcoursemanagement2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapCourseManagement2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapCourseManagement2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/in28minutes/soap/webservies/soapcoursemanagement2/SoapCourseManagement2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.soap.webservies.soapcoursemanagement2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoapCourseManagement2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
